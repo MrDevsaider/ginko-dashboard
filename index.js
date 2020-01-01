fc2d44ae4483ef7d5f40ea9fe6baf4182f98f61d
Glitch (hello-express) <none>
2020-01-01 21:00:29 +0000

    👕🎒 Checkpoint ./rutas/paginas/indice.ejs:5777510/167 ./index.js:508240/9359

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const app = require("express")(),
+/* Definimos las variables de express y passport */
+require("dotenv").config();
+var express = require("express"),
+  path = require("path"),
+  config = require("./config.js"),
   session = require("express-session"),
   passport = require("passport"),
   { Strategy } = require("passport-discord"),
-  path = require("path"),
-  config = require("./main-config.js"),
-  { Client } = require("discord.js"),
-  client = new Client();
+  app = express();
 
+// EJS
 app.engine("ejs", require("ejs").__express);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "rutas"));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+/* Control de sesiones */
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -18,18 +22,25 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-const scopes = ["indetify", "email", "guilds", "guilds.join"];
+/** Información del oAuth permitida por el usuario en la aplicación */
+var scopes = [
+  "identify",
+  "email",
+  /* 'connections', (it is currently broken) */ "guilds",
+  "guilds.join"
+];
 
+/** Lógica del callback, proporcionando la ID de la apliación y la ID secreta de autenticación */
 passport.use(
   new Strategy(
     {
       clientID: config.tokens.id,
       clientSecret: config.tokens.app,
-      callbackURL: config.tokens.url + "login/callback",
+      callbackURL: config."/callback",
       scope: scopes
     },
-    (accessToken, refreshToken, profile, done) => {
-      process.nextTick(() => {
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function() {
         return done(null, profile);
       });
     }
@@ -38,51 +49,55 @@ passport.use(
 
 app.use(
   session({
-    secret: "moneshermosa",
+    secret: "scripthub_satella",
     resave: false,
-    saveUninitialize: false
+    saveUninitialized: false
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.get("/", (req, res) => {
-  res.render("paginas/indice", {
-    monmon: "Esto es una prueba"
+app.get("/", function(req, res) {
+  let loginstatus = true;
+  if (!req.isAuthenticated()) {
+    loginstatus = false;
+  }
+  res.render("pages/indice", {
+    isLogged: {
+      link: "/logout",
+      message: "Logout",
+      status: loginstatus
+    }
   });
 });
-
 app.get("/login", passport.authenticate("discord", { scope: scopes }), function(
   req,
   res
 ) {});
-
 app.get(
-  "/login/callback",
+  "/callback",
   passport.authenticate("discord", { failureRedirect: "/" }),
-  (req, res) => {
+  function(req, res) {
     res.redirect("/perfil");
-  }
+  } // auth success
 );
-
-app.get("/logout", (req, res) => {
+app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
 });
-
-app.get("/perfil", checkAuth, (req, res) => {
+app.get("/perfil", checkAuth, function(req, res) {
+  //Manda los párametros al
   console.log(req.user);
-  res.render("paginas/perfil", {
-    userData: req.user
+  res.render("pages/perfil", {
+    userdata: req.user
   });
 });
 
 function checkAuth(req, res, next) {
-  if (req.isAuthenticated) return next();
+  if (req.isAuthenticated()) return next();
   res.redirect("/");
 }
 
-app.listen(3000, err => {
-  if (err) return console.error(err);
-  console.log("Escuchando en 3000");
+app.listen(5000, function(err) {
+  if (err) return console.log(err);
+  console.log("Escuchando en 5000");
 });
